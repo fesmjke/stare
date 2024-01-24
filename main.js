7b0f65a9633471c28cc1e6893386e983570568c2
fesmjke <fatalerror.sm@gmail.com>
2024-01-24 23:00:45 +0200

    feat: added auto opened dev tools on dev mode

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const {app, BrowserWindow} = require('electron');
 
+const isDev = process.env.NODE_ENV !== 'development';
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600
 });
+    if(isDev) {
+        win.webContents.openDevTools();
+    }
 
     win.loadFile("./public/index.html");
 }
