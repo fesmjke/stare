dd33153ca70375dcbe21821b23f1f5d87e007c36
fesmjke <fatalerror.sm@gmail.com>
2024-01-25 20:49:14 +0200

    feat: added images load from directory

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const {app, BrowserWindow, ipcMain, dialog, Menu} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const fs = require("node:fs/promises");
 const path = require('node:path');
 
+const EXT_PPM = '.ppm';
+const EXT_BMP = '.bmp';
+
 const state = {
-    directory: ""
+    directory: "",
+    images: []
+}
+
+const loadImages = async (directory) => {
+    const files = await fs.readdir(directory);
+    
+    const images_path = files.filter(file => {
+        return path.extname(file).toLowerCase() === EXT_PPM || path.extname(file).toLowerCase() === EXT_BMP 
+    }).map(file => {return path.join(directory, file)});
+    
+    return images_path
 }
 
 const selectDirectory = async () => {
     const {canceled, filePaths} = await dialog.showOpenDialog({properties: ['openDirectory']});
     if (!canceled) {
         state.directory = filePaths[0];
+        state.images = await loadImages(state.directory);
         return filePaths[0]
     }
 }
