014bc12df1d9d2354c477d5bbf3ac2af56c12f90
fesmjke <fatalerror.sm@gmail.com>
2024-01-25 18:00:17 +0200

    feat: added open dialog (to select directory)

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain, dialog, Menu} = require('electron');
+const path = require('node:path');
+
+const state = {
+    directory: ""
+}
+
+const selectDirectory = async () => {
+    const {canceled, filePaths} = await dialog.showOpenDialog({properties: ['openDirectory']});
+    if (!canceled) {
+        state.directory = filePaths[0];
+        return filePaths[0]
+    }
+}
 
 const isDev = process.env.NODE_ENV !== 'development';
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
-});
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "./src/preload.js"),
+            nodeIntegration: true
+        }
+    });
+    
     if(isDev) {
         win.webContents.openDevTools();
     }
-
     win.loadFile("./public/index.html");
 }
 
 app.whenReady().then(() => {
-    createWindow();
+    ipcMain.handle('dialog:openDirectory', selectDirectory);
+    createWindow(); 
 })
