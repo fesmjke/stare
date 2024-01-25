b0aa49ceaafb932f65a28e8f4dfa296682a48301
fesmjke <fatalerror.sm@gmail.com>
2024-01-25 23:30:42 +0200

    feat: added ability to render images

--- src/renderer.js
+++ src/renderer.js
@@ -1,15 +1,22 @@
-const imageFormats = [".ppm", ".bmp"];
-
 (() => {
+    const image_element = document.getElementById("images");
     const directory_element = document.getElementById("directory");
     const directory_select_element = document.getElementById("select-directory");
 
     directory_select_element.addEventListener('click', async (event) => {
         event.preventDefault();
 
-        const path = await window.electronAPI.openDirectory();
+        const {directory, images} = await window.electronAPI.openDirectory();
 
-        directory_element.innerText = "Selected directory : " + path;
+        directory_element.innerText = "Selected directory : " + directory;
+    
+        for (const image_path of images) {
+            const img = document.createElement('img');
+
+            img.src = image_path;
+            img.className = "img-thumbnail";
+            image_element.appendChild(img);
+        }
     })
 })();
 
