a3b509f4da9ba8abfc011d30b84644002bb7dace
fesmjke <fatalerror.sm@gmail.com>
2024-01-26 20:30:23 +0200

    ref: updated main file, removed state and make it more clean

--- main.js
+++ main.js
@@ -1,71 +1,49 @@
 const {app, BrowserWindow, ipcMain, dialog} = require('electron');
-const fs = require("node:fs/promises");
+const { loadImages } = require('./src/images');
 const chokidar = require('chokidar');
 const path = require('node:path');
-
-const EXT_PNG = '.png';
-
-const state = {
-    directory: "",
-    images: {},
-}
 
 let win;
 let observer;
 
-function startMonitoring(filePath) {
-  observer = chokidar.watch(filePath, {
-    ignored: /(^|[\/\\])\../, // ignore dotfiles
-    persistent: true,
-    alwaysStat: true
-  });
-
-  observer.on('change', (fullPath, stats) => {
-    const date = stats.mtime.toLocaleString('en-US', { timeZoneName: 'short' });
-    console.log(`File ${fullPath} changed. ${date}`);
-    const base = path.basename(fullPath);
-    win.webContents.send('file-change', {base, fullPath, date});
-  });
-
-  observer.on('unlink', (fpath) => {
-    console.log(`File ${fpath} deleted.`);
-  });
-}
-
-const loadImages = async (directory) => {
-    const files = await fs.readdir(directory);
-    
-    const image_base = files.filter(file => {
-        return path.extname(file) === EXT_PNG
+const startMonitoring = (fullpath) => {
+    observer = chokidar.watch(fullpath, {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true,
+        alwaysStat: true
     });
-    
-    const image_full_path = image_base.map((file) => {
-        return path.join(directory, file);
-    })
 
-    return {image_base, image_full_path} 
+    observer.on('change', (fullpath, stats) => {
+        const date = stats.mtime.toLocaleString('en-US', { timeZoneName: 'short' });
+        
+        console.log(`File ${fullpath} changed at ${date}`);
+        
+        const basename = path.basename(fullpath);
+        win.webContents.send('file-change', {basename, fullpath, date});
+    });
+
+    observer.on('unlink', (fullpath) => {
+        console.log(`File ${fullpath} deleted.`);
+    });
 }
 
 const selectDirectory = async () => {
     const {canceled, filePaths} = await dialog.showOpenDialog({properties: ['openDirectory']});
     if (!canceled) {
-        state.directory = filePaths[0];
-        const {image_base, image_full_path} = await loadImages(state.directory);
-
-        state.images = {
-            base: image_base,
-            path: image_full_path
-        }
-
-        image_full_path.forEach(startMonitoring);
-        return {directory: filePaths[0], image_base, image_full_path}
+        const directory = filePaths[0];
+        const {basenames, fullpaths} = await loadImages(directory);
+        
+        fullpaths.forEach(startMonitoring);
+        
+        return {directory, basenames, fullpaths}
     }
 }
 
-const isDev = process.env.NODE_ENV !== 'development';
+const isDev = process.env.NODE_ENV === 'development';
 
 const createWindow = () => {
     win = new BrowserWindow({
+        title: 'Stare',
         width: 800,
         height: 600,
         webPreferences: {
